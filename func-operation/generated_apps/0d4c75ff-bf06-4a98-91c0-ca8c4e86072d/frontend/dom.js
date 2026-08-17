1d5163e36c386f15337e8d98c59dec926667fabe
梵高 <fangao@staruniongame.com>
2026-08-17 15:18:04 +0800

    workflows

--- func-operation/generated_apps/0d4c75ff-bf06-4a98-91c0-ca8c4e86072d/frontend/dom.js
+++
@@ -1,8 +0,0 @@
-export function escapeHTML(value) {
-  return String(value)
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#39;')
-}
